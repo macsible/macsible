2385c3e0bf903750d8162477f338a7aefb893d88
Dan Bohea <git@bohea.co.uk>
2016-02-26 12:30:30 +0000

    Idempotent Time Machine defaults.

--- roles/osx/files/osx.sh
+++ roles/osx/files/osx.sh
@@ -295,7 +295,7 @@ fi
 # ------------------------------------------------------------------------------
 
 # Prevent Time Machine from prompting to use new hard drives as backup volume
-defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+# defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
 
 # Activity Monitor
 # ------------------------------------------------------------------------------
