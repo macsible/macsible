097cddcf9704c199a92423ac9ad8bef438326a9e
Dan Bohea <git@bohea.co.uk>
2018-01-07 00:37:15 +0000

    Move into root dir before creating fork files from script

--- scripts/create_fork_files.sh
+++ scripts/create_fork_files.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
 DIR="$(dirname "$0")"
+
 . "$DIR/common.sh"
+
+cd "$DIR"
+cd ../
 
 # Create requirements.yml
 setStatusMessage "Creating requirements.yml if absent"
-cp -n ../src/requirements.example.yml ../requirements.yml
+cp -n src/requirements.example.yml requirements.yml
 
 # Create mac-custom.yml
 setStatusMessage "Creating mac-custom.yml if absent"
-cp -n ../src/mac-custom.example.yml ../mac-custom.yml
+cp -n src/mac-custom.example.yml mac-custom.yml
 
 # Create config.yml
 setStatusMessage "Creating config.yml if absent"
-cp -n ../src/config.example.yml ../config.yml
+cp -n src/config.example.yml config.yml
 
 # Create config.local.yml
 setStatusMessage "Creating config.local.yml if absent"
-cp -n ../src/config.example.yml ../config.local.yml
+cp -n src/config.example.yml config.local.yml
