8d47b9f2c085f80c88b18fa3f40a72ab4a6f58dc
Dan Bohea <git@bohea.co.uk>
2018-01-07 00:20:48 +0000

    #6 Created additional scripts to handle installing deps and creating fork files

---
+++ scripts/create_fork_files.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+. common.sh
+
+# Create requirements.yml
+setStatusMessage "Creating requirements.yml if absent"
+cp -n ../src/requirements.example.yml ../requirements.yml
+
+# Create mac-custom.yml
+setStatusMessage "Creating mac-custom.yml if absent"
+cp -n ../src/mac-custom.example.yml ../mac-custom.yml
+
+# Create config.yml
+setStatusMessage "Creating config.yml if absent"
+cp -n ../src/config.example.yml ../config.yml
+
+# Create config.local.yml
+setStatusMessage "Creating config.local.yml if absent"
+cp -n ../src/config.example.yml ../config.local.yml
---
+++ scripts/install_deps.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+. common.sh
+
+# Install pip
+if ! exists pip; then
+  setStatusMessage "Installing pip"
+  sudo easy_install pip
+fi
+
+# Install Ansible
+# TODO: We need at least Ansible 2.4.0, upgrade if needed.
+if ! exists ansible; then
+  setStatusMessage "Installing Ansible"
+  sudo pip install ansible --quiet
+fi
+
+# Confirm installed versions
+pip --version
+ansible --version
