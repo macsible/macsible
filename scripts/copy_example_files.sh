bfc165e41bb07e7fdadd79d24b0d7f34cc19a365
Dan Bohea <git@bohea.co.uk>
2018-03-06 09:32:14 +0000

    DRYer copy_example_files.sh

--- scripts/copy_example_files.sh
+++ scripts/copy_example_files.sh
@@ -22,20 +22,23 @@ mkdir custom
 # when using `cp -n` (it's the `-n` it doesn't like).
 # https://github.com/travis-ci/travis-ci/issues/6307#issuecomment-363899104
 
+setStatusMessage "Creating ./custom/*.yml files if absent"
+rsync --include '*.yml' --exclude='*' --ignore-existing starter_files/ custom/
+
 setStatusMessage "Creating ./ansible.cfg if absent"
 rsync starter_files/ansible.cfg ansible.cfg --ignore-existing
 
-setStatusMessage "Creating ./custom/config.yml if absent"
-rsync starter_files/config.yml custom/config.yml --ignore-existing
+# setStatusMessage "Creating ./custom/config.yml if absent"
+# rsync starter_files/config.yml custom/config.yml --ignore-existing
 
 setStatusMessage "Creating ./custom/config.local.yml if absent"
 rsync starter_files/config.yml custom/config.local.yml --ignore-existing
 
-setStatusMessage "Creating ./custom/inventory.yml if absent"
-rsync starter_files/inventory.yml custom/inventory.yml --ignore-existing
+# setStatusMessage "Creating ./custom/inventory.yml if absent"
+# rsync starter_files/inventory.yml custom/inventory.yml --ignore-existing
 
-setStatusMessage "Creating ./custom/mac.yml if absent"
-rsync starter_files/mac.yml custom/mac.yml --ignore-existing
+# setStatusMessage "Creating ./custom/mac.yml if absent"
+# rsync starter_files/mac.yml custom/mac.yml --ignore-existing
 
-setStatusMessage "Creating ./custom/requirements.yml if absent"
-rsync starter_files/requirements.yml custom/requirements.yml --ignore-existing
+# setStatusMessage "Creating ./custom/requirements.yml if absent"
+# rsync starter_files/requirements.yml custom/requirements.yml --ignore-existing
